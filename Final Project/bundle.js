532a07e8b9c7eb4a02f58343aa50835c50eaef78
Kai Yan <kyan2@wpi.edu>
2020-03-02 14:51:40 -0500

    change default frameRate

--- Final Project/bundle.js
+++ Final Project/bundle.js
@@ -36,7 +36,7 @@ this.separationScale = 1;
 this.mouseCentral = false;
 this.mousePredator = false;
 this.gyroscope = false;
-  this.frameRate = 34;
+  this.frameRate = 60;
 
 };
 myGui = new MyGUI();
